92b8ce9b951278216a9c34f7e98b5ee76d65f9a4
ghk829 <ghk829@gmail.com>
2017-08-17 12:12:29 +0900

    plus

--- mnist/mnist_models.py
+++ mnist/mnist_models.py
@@ -42,32 +42,7 @@ class LogisticModel(models.BaseModel):
 
 class MyModel(models.BaseModel):
   """Logistic model with L2 regularization."""
-  def nielsen_net(inputs, is_training, scope='NielsenNet'):
-    with tf.variable_scope('NielsenNet'):
-        # First Group: Convolution + Pooling 28x28x1 => 28x28x20 => 14x14x20
-        net = slim.conv2d(inputs, 20, [5, 5], padding='SAME', scope='layer1-conv')
-        net = slim.max_pool2d(net, 2, stride=2, scope='layer2-max-pool')
 
-        # Second Group: Convolution + Pooling 14x14x20 => 10x10x40 => 5x5x40
-        net = slim.conv2d(net, 40, [5, 5], padding='VALID', scope='layer3-conv')
-        net = slim.max_pool2d(net, 2, stride=2, scope='layer4-max-pool')
-
-        # Reshape: 5x5x40 => 1000x1
-        net = tf.reshape(net, [-1, 5*5*40])
-
-        # Fully Connected Layer: 1000x1 => 1000x1
-        net = slim.fully_connected(net, 1000, scope='layer5')
-        net = slim.dropout(net, is_training=is_training, scope='layer5-dropout')
-
-        # Second Fully Connected: 1000x1 => 1000x1
-        net = slim.fully_connected(net, 1000, scope='layer6')
-        net = slim.dropout(net, is_training=is_training, scope='layer6-dropout')
-
-        # Output Layer: 1000x1 => 10x1
-        net = slim.fully_connected(net, 10, scope='output')
-        net = slim.dropout(net, is_training=is_training, scope='output-dropout')
-
-	return net
   def create_model(self, model_input, num_classes=10, l2_penalty=1e-8, **unused_params):
     """Creates a logistic model.
 
@@ -79,5 +54,10 @@ class MyModel(models.BaseModel):
       A dictionary with a tensor containing the probability predictions of the
       model in the 'predictions' key. The dimensions of the tensor are
       batch_size x num_classes."""
-    net = self.nielsen_net(model_input,True)
-    return {"predictions": net}
+    net = slim.conv2d(model_input, 20, [5, 5], padding='SAME', scope='layer1-conv')
+    net = slim.max_pool2d(net, 2, stride=2, scope='layer2-max-pool')
+
+    output = slim.fully_connected(
+        net, num_classes, activation_fn=tf.nn.softmax,
+        weights_regularizer=slim.l2_regularizer(l2_penalty))
+    return {"predictions": output}
